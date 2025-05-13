29da942c987e341303cdba0a706bd7f70e6c6fcf
Ruhmer Jairus <espinajairus@gmail.com>
2025-05-13 21:37:10 +0800

    MODIFIED WIZARD DIFFICULTY

--- src/main/java/com/oop2/typewiz/GameplayComponents/Game.java
+++ src/main/java/com/oop2/typewiz/GameplayComponents/Game.java
@@ -160,8 +160,8 @@ public class Game extends GameApplication {
                 maxActiveEntities = 10;
                 waveSpawnsPerWave = new int[]{6, 7, 8, 9, 10, 11, 12, 13};
                 waveSpeedMultipliers = new double[]{0.8, 0.8, 0.5, 1.1, 1.0, 1.0, 1.0, 1.3};
-                minSpawnsPerGroupByWave = new int[]{1, 1, 2, 2, 2, 3, 3, 3};
-                maxSpawnsPerGroupByWave = new int[]{2, 2, 3, 3, 4, 4, 5, 5};
+                minSpawnsPerGroupByWave = new int[]{1, 1, 2, 2, 2, 2, 2, 2};
+                maxSpawnsPerGroupByWave = new int[]{2, 2, 2, 2, 2, 2, 3, 3};
                 spawnDelayMultipliers = new double[]{1.0, 0.95, 0.95, 0.95, 0.8, 0.8, 0.8, 0.7};
                 break;
             
@@ -170,7 +170,7 @@ public class Game extends GameApplication {
                 maxActiveEntities = 18;
                 waveSpawnsPerWave = new int[]{10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32};
                 waveSpeedMultipliers = new double[]{1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.2};
-                minSpawnsPerGroupByWave = new int[]{1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5};
+                minSpawnsPerGroupByWave = new int[]{1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3};
                 maxSpawnsPerGroupByWave = new int[]{2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8};
                 spawnDelayMultipliers = new double[]{0.9, 0.85, 0.8, 0.75, 0.7, 0.65, 0.6, 0.55, 0.5, 0.45, 0.4, 0.35};
                 break;
